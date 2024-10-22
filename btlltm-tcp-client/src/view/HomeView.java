1f91112405547d3062408c057d9bb1dbb0d87129
quang2719 <111571873+quang2719@users.noreply.github.com>
2024-10-23 02:02:45 +0700

    add color

--- btlltm-tcp-client/src/view/HomeView.java
+++ btlltm-tcp-client/src/view/HomeView.java
@@ -136,7 +136,7 @@ public class HomeView extends javax.swing.JFrame {
         infoUserScore.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
         infoUserScore.setText("Score");
 
-        btnLogout.setBackground(new java.awt.Color(255, 51, 0));
+        btnLogout.setBackground(new java.awt.Color(204, 0, 0));
         btnLogout.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
         btnLogout.setForeground(new java.awt.Color(204, 255, 255));
         btnLogout.setText("Logout");
@@ -156,7 +156,7 @@ public class HomeView extends javax.swing.JFrame {
             }
         });
 
-        btnExit.setBackground(new java.awt.Color(255, 51, 0));
+        btnExit.setBackground(new java.awt.Color(204, 0, 0));
         btnExit.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
         btnExit.setForeground(new java.awt.Color(204, 255, 255));
         btnExit.setText("EXIT");
